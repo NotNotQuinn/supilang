233d651bea1c992ef1aa1115757dd89531bbc952
Quinn T <Quinn.GitHub@gmail.com>
2022-02-04 04:24:26 -0600

    Refactor from custom parser to parser package

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/notnotquinn/supilang
 
 go 1.17
+
+require (
+	github.com/alecthomas/participle/v2 v2.0.0-alpha7
+	github.com/alecthomas/repr v0.0.0-20181024024818-d37bc2a10ba1
+)
--- main.go
+++ main.go
@@ -5,6 +5,10 @@ import (
 	"log"
 	"os"
 	"strings"
+
+	"github.com/alecthomas/participle/v2"
+	"github.com/alecthomas/participle/v2/lexer"
+	"github.com/alecthomas/repr"
 )
 
 type OpType uint64
@@ -64,21 +68,55 @@ func (p *Program) Compile() string {
 	}
 }
 
+// Custom lexer for Aliases
+var aliasLexer = lexer.MustSimple([]lexer.Rule{
+	{`Ident`, `[-a-zA-Z_0-9]{2,30}`, nil},
+	// {`Word`, `[a-zA-Z_][a-zA-Z0-9_]`, nil},
+	{`Keyword`, `alias|end|\||exec|pipe`, nil},
+	{`CommandLiteral`, `"(?:\\.|[^"])*"`, nil},
+	{"comment", `#[^\n]*`, nil},
+	{"whitespace", `\s+`, nil},
+})
+
+var parser = participle.MustBuild(&Alias{},
+	participle.Lexer(aliasLexer),
+	participle.Unquote("CommandLiteral"),
+)
+
+type Alias struct {
+	Name string              `"alias" @Ident`
+	Body []*CommandExecution `@@* "end"`
+}
+
+type CommandExecution struct {
+	ExecCommandLiteral  *string   `  "exec" @CommandLiteral`
+	PipeCommandLiterals *[]string `| "pipe" @CommandLiteral { "|" @CommandLiteral }`
+}
+
+func (a *Alias) CompileToOps() []Operation {
+	ops := []Operation{}
+	for _, ce := range a.Body {
+		if ce.PipeCommandLiterals != nil {
+			ops = append(ops, Operation{OP_PIPE, *ce.PipeCommandLiterals})
+		} else if ce.ExecCommandLiteral != nil {
+			ops = append(ops, Operation{OP_EXEC, *ce.ExecCommandLiteral})
+		}
+	}
+	return ops
+}
+
 func main() {
 	bytes, err := os.ReadFile("something.supilang")
 	if err != nil {
 		log.Fatal("open: ", err)
 	}
-
-	lexer := NewLexer()
-	tokens := lexer.Lex(string(bytes))
-	fmt.Println(tokens)
-	parser := NewParser(string(bytes))
-	result, err := parser.Parse()
+	alias := &Alias{}
+	err = parser.ParseBytes("something.supilang", bytes, alias)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	p := Program{result.GetOperations()}
+	repr.Println(alias, repr.Indent("  "), repr.OmitEmpty(true))
+	fmt.Println(alias.CompileToOps())
+	p := (Program{alias.CompileToOps()})
 	fmt.Println(p.Compile())
 }
